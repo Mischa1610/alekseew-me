da8d6eb0c6995fd65ea6600dc754727c576639e3
Michael Alekseew <alekseew.m@googlemail.com>
2019-06-03 00:50:21 +0200

    build(app): add Angular Material and set it up

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { AppApiModule } from '@app/app-api.module';
-import { AppRoutingModule } from '@app/app-routing.module';
-import { AppComponent } from '@app/app/app.component';
 import { CoreModule } from '@core/core.module';
 import { environment } from '@env/environment';
 import { SharedModule } from '@shared/shared.module';
+import { AppApiModule } from './app-api.module';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app/app.component';
 
 @NgModule({
     declarations: [AppComponent],
@@ -16,7 +17,8 @@ import { SharedModule } from '@shared/shared.module';
         CoreModule,
         SharedModule,
         AppApiModule,
-        AppRoutingModule
+        AppRoutingModule,
+        BrowserAnimationsModule
     ],
     bootstrap: [AppComponent]
     /* NO PROVIDERS HERE! ALL SERVICES SHOULD BE IN PROVIDED-IN APP-API.MODULE*/
